377c1846af583605cda1b12650856f74493c5ee3
AkshayN01 <nambly77@gmail.com>
2024-06-28 02:37:13 +0100

    added register component

---
+++ src/backend/DevelopersDen/DevelopersDen.Client/src/app/job-seeker/components/register/register.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]],
+      phoneNumber: ['']
+    }, { validator: this.passwordMatchValidator });
+  }
+
+  passwordMatchValidator(form: FormGroup) {
+    return form.get('password')!.value === form.get('confirmPassword')!.value 
+      ? null : { 'mismatch': true };
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+    }
+  }
+}
