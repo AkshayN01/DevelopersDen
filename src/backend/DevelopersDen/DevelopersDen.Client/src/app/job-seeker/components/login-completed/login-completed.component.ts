13d81d4fb8ae2398c1757f6bbf637bcbc23d4387
AkshayN01 <nambly77@gmail.com>
2024-06-28 02:37:55 +0100

    added login-completed component

---
+++ src/backend/DevelopersDen/DevelopersDen.Client/src/app/job-seeker/components/login-completed/login-completed.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-completed',
+  templateUrl: './login-completed.component.html',
+  styleUrl: './login-completed.component.css'
+})
+export class LoginCompletedComponent {
+
+}
