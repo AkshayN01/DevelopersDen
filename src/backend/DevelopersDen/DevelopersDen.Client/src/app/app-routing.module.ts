6893bebb2b0b2490f7386ea4a0dba45db8d91163
AkshayN01 <nambly77@gmail.com>
2024-06-28 02:39:59 +0100

    Updated routes

--- src/backend/DevelopersDen/DevelopersDen.Client/src/app/app-routing.module.ts
+++ src/backend/DevelopersDen/DevelopersDen.Client/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { JobSeekerComponent } from './job-seeker/job-seeker.component';
+import { LoginComponent } from './job-seeker/components/login/login.component';
+import { RegisterComponent } from './job-seeker/components/register/register.component';
+import { HeaderComponent } from './job-seeker/components/protected/header/header.component';
+import { ProfileComponent } from './job-seeker/components/protected/profile/profile.component';
+import { HomeComponent } from './job-seeker/components/protected/home/home.component';
+import { JobApplicationsComponent } from './job-seeker/components/protected/job-applications/job-applications.component';
+import { LoginCompletedComponent } from './job-seeker/components/login-completed/login-completed.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'jobSeeker', component: JobSeekerComponent, children: [
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'login-completed', component: LoginCompletedComponent },
+    { path: '', component: HeaderComponent, children: [
+      { path: 'profile', component: ProfileComponent },
+      { path: 'home', component: HomeComponent },
+      { path: 'my-applications', component: JobApplicationsComponent }
+    ] }
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
