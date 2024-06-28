6cf4e35b9c391f7e71ab98d7de91b857e2e4664b
AkshayN01 <nambly77@gmail.com>
2024-06-28 02:33:33 +0100

    Added profile for mapper

--- src/backend/DevelopersDen/DevelopersDen.Library/Profiles/JobSeekerAMProfile.cs
+++ src/backend/DevelopersDen/DevelopersDen.Library/Profiles/JobSeekerAMProfile.cs
@@ -13,7 +13,24 @@ namespace DevelopersDen.Library.Profiles
     {
         public JobSeekerAMProfile() 
         {
-            CreateMap<JobSeeker, JobSeekerDTO>();
+            CreateMap<JobSeeker, JobSeekerDTO>()
+                .ForMember(dest => dest.Guid, src => src.MapFrom(x => x.JobSeekerId))
+                .ForMember(dest => dest.Name, src => src.MapFrom(x => x.JobSeekerId))
+                .ForMember(dest => dest.EmailId, src => src.MapFrom(x => x.JobSeekerId))
+                .ForMember(dest => dest.PhoneNumber, src => src.MapFrom(x => x.JobSeekerId));
+
+            CreateMap<JobSeekerProfile, JobSeekerProfileDTO>()
+                .ForMember(dest => dest.JobSeekerProfileId, src => src.MapFrom(x => x.JobSeekerId))
+                .ForMember(dest => dest.Resume, src => src.MapFrom(x => x.JobSeekerId))
+                .ForMember(dest => dest.Summary, src => src.MapFrom(x => x.JobSeekerId));
+
+            CreateMap<WorkExperience, WorkExperienceDTO>()
+                .ForMember(dest => dest.CompanyName, src => src.MapFrom(x => x.CompanyName))
+                .ForMember(dest => dest.Designation, src => src.MapFrom(x => x.Designation))
+                .ForMember(dest => dest.StartDate, src => src.MapFrom(x => x.StartDate))
+                .ForMember(dest => dest.EndDate, src => src.MapFrom(x => x.EndDate))
+                .ForMember(dest => dest.WorkDescription, src => src.MapFrom(x => x.WorkDescription))
+                .ForMember(dest => dest.IsCurrent, src => src.MapFrom(x => x.IsCurrent));
         }
     }
 }
